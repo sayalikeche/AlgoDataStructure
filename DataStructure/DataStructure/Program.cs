d93b143601c4c83d345253f1faeae1542f23cdf0
sayalikeche <kechesayali11@gmail.com>
2021-11-27 07:15:42 +0530

    [sayali added UnOrderList]

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructure;
+using DS_Algorithm;
+using System;
 
 namespace DataStructure
 {
@@ -6,8 +8,25 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Structure");
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the Program number which is to be executed \n 1. Unordered List \n 2. Ordered List  \n 3. Simple Balance Parethesis \n 4. Bank Cash Counter \n 5. PallinDrome-Checker  \n 6. Hashing Function to search  \n 7. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        UnOrderedList list = new UnOrderedList();
+                        list.UnOrdered();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
         }
     }
+
 }
